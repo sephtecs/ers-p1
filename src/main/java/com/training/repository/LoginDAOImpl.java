ce51b27ff0a3e0b2df9fc881946fad0a65cf3a4f
Joseph <josephtecson18@gmail.com>
2022-03-18 00:17:14 -0700

    first commit

---
+++ src/main/java/com/training/repository/LoginDAOImpl.java
@@ -0,0 +1,113 @@
+package com.training.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.model.User;
+import com.training.util.DBConnection;
+
+public class LoginDAOImpl implements LoginDAO{
+	
+	Connection connection = DBConnection.getConnection();
+	@Override
+	public boolean register(User user) {
+		System.out.println("##Adding user :" + user);
+		PreparedStatement statement = null;
+		int rows = 0;
+		try {
+			statement = connection.prepareStatement("insert into userss values(default,?,?,?,?,?)");
+			statement.setString(1, user.getUsername());
+			statement.setString(2, user.getPassword());
+			statement.setString(3, user.getGender());
+			statement.setString(4, user.getNotifications());
+			statement.setString(5, user.getQualification());
+
+			rows = statement.executeUpdate();
+			System.out.println(rows + " user registered successfully");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (rows == 0)
+			return false;
+		else
+			return true;
+	}
+
+	@Override
+	public boolean validate(String username, String password) {
+		boolean userValid = false;
+		PreparedStatement stat;
+		try {
+			stat = connection.prepareStatement("select * from userss where username = ? and password = ? ");
+			stat.setString(1, username);
+			stat.setString(2, password);
+
+			ResultSet res = stat.executeQuery();
+			userValid = res.next();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userValid;
+	}
+
+	@Override
+	public List<User> getUsers() {
+		System.out.println("##Printing all users  ");
+		List<User> users = new ArrayList<User>();
+
+		Statement stat;
+		try {
+			stat = connection.createStatement();
+			ResultSet res = stat.executeQuery("select * from userss");
+			while (res.next()) {
+				User user = new User();
+				user.setUserId(res.getInt(1));
+				user.setUsername(res.getString(2));
+				user.setPassword(res.getString(3));
+				user.setGender(res.getString(4));
+				user.setNotifications(res.getString(5));
+				user.setQualification(res.getString(6));
+				users.add(user);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return users;
+	}
+	
+		@Override
+		public List<User> getUsersByUserName(String username) {
+			System.out.println("##Printing search users  ");
+			List<User> users = new ArrayList<User>();
+
+			Statement stat;
+			try {
+				stat = connection.createStatement();
+				ResultSet res = stat.executeQuery("select * from userss where username = '"+username+"'");
+				while (res.next()) {
+					User user = new User();
+					user.setUserId(res.getInt(1));
+					user.setUsername(res.getString(2));
+					user.setPassword(res.getString(3));
+					user.setGender(res.getString(4));
+					user.setNotifications(res.getString(5));
+					user.setQualification(res.getString(6));
+					users.add(user);
+
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return users;
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home Page</title>
+<link type="text/css" rel="stylesheet" href="styles.css">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	crossorigin="anonymous">
+<script type="text/javascript">
+	function validate() {
+		var uname = document.getElementById("username").value;
+		var pwd = document.getElementById("password").value;
+		if (uname.length == 0) {
+			alert("Please enter username")
+			return false;
+		} else if (pwd.length == 0) {
+			alert("Please enter password")
+			return false;
+		}
+		else if (pwd.length < 6 || pwd.length > 12) {
+			alert("Password length must be between 6-12")
+			return false;
+		}
+	}
+</script>
+</head>
+<body>
+	<!-- 	<h1>Login Page</h1>
+	<hr /> -->
+	<h2 class="notes">Welcome to the ERS Login page!</h2>
+	<form class="form-style" action="LoginController"
+		onsubmit="return validate()" method="post">
+		<table>
+			<tr>
+				<td><label> Username: </label></td>
+				<td><input type="text" size="31" name="username" id="username"
+					placeholder="please enter username"></td>
+			</tr>
+			<tr>
+				<td><label> Password: </label></td>
+				<td><input type="password" name="password" id="password">
+					<input type="submit" value="Sign in" id="btnSubmit"></td>
+			</tr>
+		</table>
+	</form>
+
+</body>
+</html>
